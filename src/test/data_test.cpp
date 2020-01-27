0246de5c4a958e8b6fc6b9e62cbc90ef909e3946
szotaksoma <szotaksoma@gmail.com>
2020-01-27 16:20:58 +0100

    Changed data_test.cpp

--- src/test/data_test.cpp
+++ src/test/data_test.cpp
@@ -11,17 +11,22 @@ int main(int argc, const char** argv) {
 		Debug::suppress_errors(true);
 	}
 
-	double numbers[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+	double twos[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+	double threes[] = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
 
-	Series series = Series("numbers");
-	series.from_array(numbers, sizeof(numbers)/sizeof(numbers[0]));
-	series.head();
+	Series s1 = Series("2's multiples");
+	Series s2 = Series("3's multiples");
+
+	s1.from_array(twos, sizeof(twos) / sizeof(twos[0]));
+	s2.from_array(threes, sizeof(threes) / sizeof(threes[0]));
 
 	Frame df;
-	df.add(series);
+	df.add(s1);
+	df.add(s2);
 
-	df["numbers"].head();
-	Debug::info(to_string(df["numbers"][2]));
+	df["2's multiples"].head();
+	df["3's multiples"].head();
+	Debug::info(to_string(df["2's multiples"][5]));
 
 	return 0;
 
